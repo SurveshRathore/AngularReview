7da7b95f6ea6b67a0e5c526df989e40d2a28cb5e
Survesh Rathore <113424413+SurveshRathore@users.noreply.github.com>
2023-06-28 13:17:22 +0530

    Create parentcomponent.component.ts

---
+++ parentChildData/parentcomponent/parentcomponent.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-parentcomponent',
+  templateUrl: './parentcomponent.component.html',
+  styleUrls: ['./parentcomponent.component.scss']
+})
+export class ParentcomponentComponent {
+parentForm! : FormGroup;
+userData: any = [];
+userValue: any;
+
+ constructor(private formBuilder: FormBuilder) {}
+
+ ngOnInit(){
+   this.parentForm = this.formBuilder.group({
+     userValue: ['', Validators.required]
+   })
+ }
+
+ get f(){ return this.parentForm.controls}
+
+
+ onSubmit(){
+   console.log(this.parentForm.value)
+   this.userData.push(this.parentForm.value);
+   console.log(this.userData)
+ }
+
+
+
+}
