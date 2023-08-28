68a794e7b6276009ec4029586febb2e72f487240
Survesh Rathore <113424413+SurveshRathore@users.noreply.github.com>
2023-08-28 16:19:31 +0530

    applying ng on changes

--- lifecycle/ang-life/ang-life.component.ts
+++ lifecycle/ang-life/ang-life.component.ts
@@ -10,21 +10,9 @@ export class AngLifeComponent implements OnChanges {
   @Input() data: number = 0;
   @Input() data2: boolean = false;
 
-  constructor(){
-    console.log("Hello from Child using constructor");
-  }
-
 ngOnChanges(changes: SimpleChanges): void {
   console.log(changes);
   
 }
-
-ngOnInit(){
-  console.log("Hello from Child using ngOnInit");
-  
-}
-
-
-
 
 }
